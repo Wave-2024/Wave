ef8e7f312360820f20179877cef95a6b5fd7fb03
Alpha17-2 <alphaisgod1@gmail.com>
2021-12-31 18:20:44 +0530

    userModel update

--- lib/models/userModel.dart
+++ lib/models/userModel.dart
@@ -1,13 +1,17 @@
 class userModel {
+  final String uid;
   final String title;
   final String username;
   final String email;
   final String bio;
   final String dp;
+  final String coverImage;
   final List<String> followers;
   final List<String> followings;
   userModel(
       {required this.title,
+      required this.coverImage,
+      required this.uid,
       required this.username,
       required this.email,
       required this.bio,
