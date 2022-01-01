2d3e1af1f4574185d5567fcb1aab3ec61d7d6a9d
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-01 22:27:17 +0530

    minor change

--- lib/utils/firebaseServices.dart
+++ lib/utils/firebaseServices.dart
@@ -1,23 +1,26 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:nexus/models/PostModel.dart';
 import 'package:nexus/utils/constants.dart';
 import 'package:http/http.dart' as http;
 
-Future<void> createNewNexusUser(String email,String title,String username,String uid) async {
+Future<void> createNewNexusUser(
+    String email, String title, String username, String uid) async {
   final String api = constants().fetchApi + 'users/$uid.json';
   try {
-    await http.put(Uri.parse(api), body: json.encode({
-      'title' : title,
-      'username' : username,
-      'email' : email,
-       'bio' : '',
-       'dp' : '',
-       'uid' : '',
-       'coverImage' : '',
-       'followers' : [],
-       'followings' : [],
-    }));
+    await http.put(Uri.parse(api),
+        body: json.encode({
+          'title': title,
+          'username': username,
+          'email': email,
+          'bio': '',
+          'dp': '',
+          'uid': '',
+          'coverImage': '',
+          'followers': [],
+          'followings': [],
+        }));
   } catch (error) {
     print(error);
   }
