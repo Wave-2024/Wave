9d805362be634dfbfdcbda4aa4ba0db1a38b64c9
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-04 20:47:55 +0530

    Post model created

--- lib/models/PostModel.dart
+++ lib/models/PostModel.dart
@@ -5,22 +5,25 @@ class PostModel {
   final String image;
   final String uid;
   final String post_id;
-  final int likes;
+  final List<String> likes;
+  final List<dynamic> comments;
 
   PostModel(
       {required this.caption,
       required this.image,
+      required this.comments,
       required this.uid,
       required this.post_id,
       required this.likes});
 
   Map<String, dynamic> toJson() {
     return {
-      'caption' : caption,
-      'image' : image,
-      'uid' : uid,
-      'post_uid' :post_id,
-      'likes' : likes,
+      'caption': caption,
+      'image': image,
+      'uid': uid,
+      'post_uid': post_id,
+      'likes': likes,
+      'comments': comments, 
     };
   }
 }
