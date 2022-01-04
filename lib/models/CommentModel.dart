ddde857deb2968961119b76802001d1d10eb61e6
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-04 20:48:32 +0530

    Model class for comment

---
+++ lib/models/CommentModel.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class CommentModel {
+  final String uid;
+  final String comment;
+
+  CommentModel({
+    required this.comment,
+    required this.uid,
+  });
+}
