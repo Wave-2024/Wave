15bbdea88775849b3dd1017cfa73f8bda72c9f47
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-07 01:04:24 +0530

    fullstack implementation of the screen

--- lib/screen/feedScreen.dart
+++ lib/screen/feedScreen.dart
@@ -1,19 +1,59 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:nexus/models/PostModel.dart';
+import 'package:nexus/providers/manager.dart';
 import 'package:nexus/services/AuthService.dart';
 import 'package:nexus/utils/devicesize.dart';
+import 'package:nexus/utils/widgets.dart';
+import 'package:provider/provider.dart';
 
-class feedScreen extends StatelessWidget {
+class feedScreen extends StatefulWidget {
+  @override
+  State<feedScreen> createState() => _feedScreenState();
+}
+
+class _feedScreenState extends State<feedScreen> {
   final authservice _auth = authservice(FirebaseAuth.instance);
+  User? currentUser;
+  bool init = true;
+  bool? loadScreen;
+  @override
+  void initState() {
+    loadScreen = true;
+    currentUser = FirebaseAuth.instance.currentUser;
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() async {
+    if(init){
+      Provider.of<usersProvider>(context).setFeedPosts(currentUser!.uid.toString()).then((value) {
+        loadScreen = false;
+        init = false;
+      });
+    }
+    super.didChangeDependencies();
+
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<PostModel> feedPosts = Provider.of<usersProvider>(context).fetchPostsToDisplay;
+    final Map<String,dynamic> mapOfUsers = Provider.of<usersProvider>(context).fetchMapOfUsers;
     return Scaffold(
       body: SafeArea(
         child: Container(
           height: displayHeight(context),
           width: displayWidth(context),
           color: Colors.white,
-          child: Column(
+          child: (loadScreen!) ?
+          const Center(
+            child: CircularProgressIndicator(
+              color: Colors.orange,
+              backgroundColor: Colors.blue,
+            ),
+          )
+              :Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
               Container(
@@ -57,151 +97,19 @@ class feedScreen extends StatelessWidget {
                 ),
               ),
               const Opacity(opacity: 0.0, child: Divider()),
-              Padding(
-                padding: const EdgeInsets.all(21.0),
-                child: Container(
-                  height: displayHeight(context) * 0.7,
-                  width: displayWidth(context),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25),
-                    color: Colors.grey[200],
-                  ),
-                  child: Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25),
-                        color: Colors.white,
-                      ),
-                      height: displayHeight(context) * 0.65,
-                      width: displayWidth(context) * 0.8,
-                      child: Padding(
-                        padding: const EdgeInsets.all(12.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    ClipRRect(
-                                      borderRadius: BorderRadius.circular(15),
-                                      child: Image.asset(
-                                        'images/male.jpg',
-                                        height: displayHeight(context) * 0.075,
-                                        width: displayWidth(context) * 0.175,
-                                      ),
-                                    ),
-                                    const VerticalDivider(),
-                                    Text(
-                                      'alpha17-2',
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize:
-                                              displayWidth(context) * 0.042,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                  ],
-                                ),
-                                IconButton(
-                                    onPressed: () {}, icon: Icon(Icons.chat))
-                              ],
-                            ),
-                            Opacity(
-                              opacity: 0.0,
-                              child: Divider(
-                                height: displayHeight(context) * 0.022,
-                              ),
-                            ),
-                            Center(
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(25),
-                                child: Image.asset(
-                                  'images/radhe.jpg',
-                                  height: displayHeight(context) * 0.42,
-                                  width: displayWidth(context) * 0.68,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                            //Divider(),
-                            Padding(
-                              padding: const EdgeInsets.only(
-                                  left: 10.0, right: 10.0),
-                              child: Container(
-                                height: displayHeight(context) * 0.075,
-                                width: displayWidth(context) * 0.8,
-                                color: Colors.transparent,
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                      children: [
-                                        CircleAvatar(
-                                          backgroundColor: Colors.transparent,
-                                          radius: displayWidth(context) * 0.04,
-                                          child: Center(
-                                            child: Image.asset(
-                                              'images/like.png',
-                                              height: displayHeight(context) *
-                                                  0.035,
-                                            ),
-                                          ),
-                                        ),
-                                        const Opacity(
-                                            opacity: 0.0,
-                                            child: VerticalDivider()),
-                                        CircleAvatar(
-                                          backgroundColor: Colors.transparent,
-                                          radius: displayWidth(context) * 0.04,
-                                          child: Center(
-                                            child: Image.asset(
-                                              'images/comment.png',
-                                              height: displayHeight(context) *
-                                                  0.035,
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                    CircleAvatar(
-                                      backgroundColor: Colors.transparent,
-                                      radius: displayWidth(context) * 0.04,
-                                      child: Center(
-                                        child: Image.asset(
-                                          'images/bookmark.png',
-                                          height:
-                                              displayHeight(context) * 0.035,
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 12.0),
-                              child: Text(
-                                '5012 likes',
-                                style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: displayWidth(context) * 0.035,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+              Container(
+                //color: Colors.blue,
+                height: displayHeight(context)*0.8,
+                width: displayWidth(context),
+                child: Padding(
+                  padding: const EdgeInsets.only(left:21.0,right: 21,top: 10),
+                  child: ListView.builder(
+                    physics: const AlwaysScrollableScrollPhysics(),
+                    itemBuilder: (context, index) {
+                    return displayPosts(context, feedPosts[index], mapOfUsers);
+                  },
+                    itemCount: feedPosts.length,
+                  )
                 ),
               )
             ],
