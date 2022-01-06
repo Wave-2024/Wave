5056d04a94b1f8ea060db254392cd5460015af61
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-06 23:29:25 +0530

    Implemented method to add new posts

--- lib/providers/manager.dart
+++ lib/providers/manager.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-
+import 'dart:io';
+import 'dart:math';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:nexus/models/PostModel.dart';
@@ -8,6 +9,42 @@ import 'package:nexus/utils/constants.dart';
 import 'package:http/http.dart' as http;
 
 class usersProvider extends ChangeNotifier {
+  List<PostModel> postsToDisplay = [];
+
+  List<PostModel> get fetchPostsToDisplay {
+    return [...postsToDisplay];
+  }
+
+  Future<void> setFeedPosts(String myUid) async {
+    print('reached setFeed Post Function');
+    List<dynamic> myFollowings = [];
+    List<dynamic> tempPosts = [];
+    List<PostModel> finalPosts = [];
+    final String api = constants().fetchApi + 'users/${myUid}.json';
+    final response = await http.get(Uri.parse(api));
+    final user = json.decode(response.body) as Map<String, dynamic>;
+    myFollowings = user['followings'] ?? [];
+    for (int index = 0; index < myFollowings.length; ++index) {
+      print('reached inside following loop');
+      final String api2 =
+          constants().fetchApi + 'users/${myFollowings[index].toString()}.json';
+      final response2 = await http.get(Uri.parse(api2));
+      final userData = json.decode(response2.body) as Map<String, dynamic>;
+      tempPosts = userData['posts'] ?? [];
+      for (int run = 0; run < tempPosts.length; ++run) {
+        finalPosts.add(PostModel(
+            caption: tempPosts[run]['caption'],
+            image: tempPosts[run]['image'],
+            comments: tempPosts[run]['comments'] ?? [],
+            uid: tempPosts[run]['uid'],
+            post_id: tempPosts[run]['post_id'],
+            likes: tempPosts[run]['likes'] ?? []));
+      }
+    }
+    postsToDisplay = finalPosts;
+    notifyListeners();
+  }
+
   List<NexusUser> searchUsers = [];
   NexusUser? currentUser;
   Map<String, NexusUser> followers =
@@ -111,7 +148,7 @@ class usersProvider extends ChangeNotifier {
     return currentUser;
   }
 
-  Future<void> removeFollower(String myUid , String personUid) async {
+  Future<void> removeFollower(String myUid, String personUid) async {
     NexusUser myProfile;
     NexusUser peopleProfile;
     List<dynamic> followings;
@@ -164,15 +201,38 @@ class usersProvider extends ChangeNotifier {
     }
   }
 
-  Future<void> addNewPostTest(PostModel post, String uid) async {
+  Future<void> addNewPost(String caption, String uid, File image) async {
     final String api = constants().fetchApi + 'users/${uid}.json';
-    final List<dynamic> posts = currentUser!.posts;
-    posts.add(post.toJson());
-    http
-        .patch(Uri.parse(api), body: json.encode({'posts': posts}))
-        .then((value) {
-      print(value.body.toString());
-    });
+    List<dynamic> posts = [];
+    try {
+      var random = Random();
+      int random1 = random.nextInt(999999);
+      int random2 = random.nextInt(555555);
+      int random3 = random.nextInt(101);
+      int random4 = random.nextInt(540);
+      final String name = '${random1}${random2}${random3}${random4}';
+      final String location = '${uid}${name}';
+      final Reference storageReference =
+          FirebaseStorage.instance.ref().child(location);
+      final UploadTask uploadTask = storageReference.putFile(image);
+      final TaskSnapshot taskSnapshot = await uploadTask;
+      taskSnapshot.ref.getDownloadURL().then((value) async {
+        PostModel post = PostModel(
+            caption: caption,
+            image: value,
+            comments: [],
+            uid: uid,
+            post_id: '',
+            likes: []);
+        final response = await http.get(Uri.parse(api));
+        final data = json.decode(response.body) as Map<String, dynamic>;
+        posts = data['posts'] ?? [];
+        posts.add(post.toJson());
+        await http.patch(Uri.parse(api), body: json.encode({'posts': posts}));
+      });
+    } catch (error) {
+      print(error);
+    }
   }
 
   Future<void> fetchUser(String uid) async {
