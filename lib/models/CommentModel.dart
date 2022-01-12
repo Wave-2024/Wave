4068163e92f48cc2c254f31b50b8c0f1438d1384
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-12 23:37:22 +0530

    modified CommentModel class' parameters

--- lib/models/CommentModel.dart
+++ lib/models/CommentModel.dart
@@ -3,9 +3,15 @@ import 'package:flutter/cupertino.dart';
 class CommentModel {
   final String commentId;
   final String uid;
+  final String userDp;
+  final String userName;
   final String comment;
+  final String dateOfComment;
 
   CommentModel({
+    required this.userDp,
+    required this.userName,
+    required this.dateOfComment,
     required this.commentId,
     required this.comment,
     required this.uid,
