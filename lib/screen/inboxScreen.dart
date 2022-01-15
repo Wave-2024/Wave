3173b08e9a1e3847d274ebe92f96af20fd9c2f01
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-15 21:43:05 +0530

    Messages - sent and receive

--- lib/screen/inboxScreen.dart
+++ lib/screen/inboxScreen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:nexus/models/userModel.dart';
 import 'package:nexus/providers/manager.dart';
 import 'package:nexus/utils/devicesize.dart';
+import 'package:nexus/utils/widgets.dart';
 import 'package:provider/provider.dart';
 
 class inboxScreen extends StatefulWidget {
@@ -14,7 +15,8 @@ class inboxScreen extends StatefulWidget {
   String? personDp;
   String? personUserName;
 
-  inboxScreen({this.myDp,this.personDp,this.personUserName,this.chatId,this.myId});
+  inboxScreen(
+      {this.myDp, this.personDp, this.personUserName, this.chatId, this.myId});
 
   @override
   State<inboxScreen> createState() => _inboxScreenState();
@@ -23,7 +25,6 @@ class inboxScreen extends StatefulWidget {
 class _inboxScreenState extends State<inboxScreen> {
   TextEditingController? messageController;
   final formKey = GlobalKey<FormState>();
-  
 
   @override
   void initState() {
@@ -32,36 +33,45 @@ class _inboxScreenState extends State<inboxScreen> {
   }
 
   @override
-  void didChangeDependencies()async {
+  void didChangeDependencies() async {
     super.didChangeDependencies();
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        iconTheme: IconThemeData(color: Colors.black,),
+        iconTheme: IconThemeData(
+          color: Colors.black,
+        ),
         title: Row(
           children: [
             CircleAvatar(
-              radius: displayWidth(context)*0.042,
+              radius: displayWidth(context) * 0.042,
               backgroundImage: NetworkImage(
                 widget.personDp.toString(),
               ),
             ),
-            const Opacity(opacity: 0,child: VerticalDivider(width: 12,)),
-            Text(widget.personUserName!,style: TextStyle(color: Colors.black),),
+            const Opacity(
+                opacity: 0,
+                child: VerticalDivider(
+                  width: 12,
+                )),
+            Text(
+              widget.personUserName!,
+              style: TextStyle(color: Colors.black),
+            ),
           ],
         ),
         actions: [
           IconButton(
-            color: Colors.black54,
+              color: Colors.black54,
               onPressed: () {
-            print('will think about it');
-          }, icon: Icon(Icons.info))
+                print('will think about it');
+              },
+              icon: Icon(Icons.info))
         ],
       ),
       body: Container(
@@ -72,30 +82,52 @@ class _inboxScreenState extends State<inboxScreen> {
           errorText: 'Comment cannot be blank',
           sendButtonMethod: () {
             if (formKey.currentState!.validate()) {
-             //sendMessage();
+              sendMessage(widget.chatId.toString(),
+                  messageController!.text.toString(), widget.myId.toString());
+              setState(() {
+                messageController!.clear();
+              });
             }
           },
           userImage: widget.myDp,
           commentController: messageController,
           labelText: "Send message",
-          sendWidget: Icon(Icons.send),
+          sendWidget: Icon(
+            Icons.send,
+            color: Colors.deepOrange,
+          ),
           textColor: Colors.black,
-          child: StreamBuilder(
-            stream: FirebaseFirestore.instance.collection(widget.chatId.toString()).orderBy('lastSent',descending: true).snapshots(),
-            builder: (context, AsyncSnapshot snapshot) {
-              if(snapshot.hasData){
-                return ListView.builder(
-                  itemCount: snapshot.data.docs.length,
-                  itemBuilder: (context, index) {
-              String message = snapshot.data.docs[index]['message'];
-              String dp = snapshot.data.docs[index]['dp'];
-                  return Text(message);
-                },);
-              }else{
-                return Center(child: Text('Say Hello to your new friend !!'),);
-              }
-
-            },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 8.0,bottom: 8,left: 16,right: 16),
+            child: StreamBuilder(
+              stream: FirebaseFirestore.instance
+                  .collection(widget.chatId.toString())
+                  .orderBy('time', descending: true)
+                  .snapshots(),
+              builder: (context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  return ListView.builder(
+                    reverse: true,
+                    //physics: NeverScrollableScrollPhysics(),
+                    //shrinkWrap: true,
+                    itemCount: snapshot.data.docs.length,
+                    itemBuilder: (context, index) {
+                      String message = snapshot.data.docs[index]['message'];
+                      String uid = snapshot.data.docs[index]['uid'];
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 10.0, ),
+                        child: messageContainer(
+                            message, uid, widget.myId.toString(), context),
+                      );
+                    },
+                  );
+                } else {
+                  return Center(
+                    child: Text('Say Hello to your new friend !!'),
+                  );
+                }
+              },
+            ),
           ),
         ),
       ),
