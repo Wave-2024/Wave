1c9b0d3aa8c2fc4cb1c4793a4ce1ca046b0e4e87
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-27 12:36:33 +0530

    Notification class model created with necessary fieds

---
+++ lib/models/NotificationModel.dart
@@ -0,0 +1,11 @@
+class NotificationModel{
+  final String? postId;
+  final String? opId;
+  final String? myUid;
+  final String? type;
+  final DateTime? time;
+  final String? notificationId;
+
+  NotificationModel({this.time,this.myUid,this.postId,this.notificationId,this.opId,this.type,});
+
+}
