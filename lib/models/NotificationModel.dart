afe8c1af377eadc5bbbfca9ea8f84666136a912e
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-28 11:41:25 +0530

    update notification status

--- lib/models/NotificationModel.dart
+++ lib/models/NotificationModel.dart
@@ -1,11 +1,15 @@
-class NotificationModel{
+class NotificationModel {
   final String? postId;
   final String? notifierUid;
   final String? type;
   final DateTime? time;
   final String? notificationId;
-  final bool? read;
+  bool? read;
 
   NotificationModel({this.read,this.time,this.notifierUid,this.postId,this.notificationId,this.type,});
+
+  updateNotificationStatus(){
+    read = true;
+  }
 
 }
