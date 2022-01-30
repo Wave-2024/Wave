5c70d9b22e6e1ca88c9a3b8d9ef815882d09fc87
Alpha17-2 <alphaisgod1@gmail.com>
2022-01-30 18:19:07 +0530

    created story model class  and upload story screen

---
+++ lib/models/StoryModel.dart
@@ -0,0 +1,7 @@
+class StoryModel {
+  final String? story;
+  final DateTime? storyTime;
+  List<dynamic>? views;
+  final String? uid;
+  StoryModel({this.story, this.uid,this.views, this.storyTime});
+}
---
+++ lib/screen/Story/uploadStory.dart
@@ -0,0 +1,110 @@
+import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:nexus/providers/manager.dart';
+import 'package:nexus/utils/devicesize.dart';
+import 'package:nexus/utils/widgets.dart';
+import 'package:provider/provider.dart';
+
+class uploadStoryScreen extends StatefulWidget {
+  final File? imageFile;
+
+  uploadStoryScreen({this.imageFile});
+
+  @override
+  State<uploadStoryScreen> createState() => _uploadStoryScreenState();
+}
+
+class _uploadStoryScreenState extends State<uploadStoryScreen> {
+  User? currentUser = FirebaseAuth.instance.currentUser;
+  bool? isUploading;
+
+  @override
+  void initState() {
+    super.initState();
+    isUploading = false;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Container(
+        height: displayHeight(context),
+        width: displayWidth(context),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: FileImage(widget.imageFile!), fit: BoxFit.contain),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: (isUploading!)
+              ? Center(
+                  child: load(context),
+                )
+              : Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          radius: displayWidth(context) * 0.06,
+                          backgroundColor: Colors.black87,
+                          child: Center(
+                            child: IconButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                color: Colors.white70,
+                                iconSize: displayWidth(context) * 0.05,
+                                icon: Icon(Icons.arrow_back_ios)),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () async {
+                            setState(() {
+                              isUploading = true;
+                            });
+                            await Provider.of<usersProvider>(context,
+                                    listen: false)
+                                .addStoryToServer(
+                                    currentUser!.uid, widget.imageFile!);
+                            setState(() {
+                              isUploading = false;
+                            });
+                            Navigator.pop(context);
+                          },
+                          child: Card(
+                            color: Colors.black87,
+                            elevation: 2,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15)),
+                            child: Padding(
+                              padding: const EdgeInsets.all(15.0),
+                              child: Center(
+                                child: Text(
+                                  'Set Story',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: displayWidth(context) * 0.036,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+        ),
+      ),
+    ));
+  }
+}
