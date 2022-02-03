a86d4c2fa4794c71d6d5ba22dc29e6711d2bfab4
Alpha17-2 <alphaisgod1@gmail.com>
2022-02-04 02:01:36 +0530

    new : global var provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:nexus/providers/GlobalVariable.dart';
 import 'package:nexus/providers/screenIndexProvider.dart';
 import 'package:nexus/providers/manager.dart';
 import 'package:nexus/providers/usernameProvider.dart';
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
         providers: [
+          ChangeNotifierProvider(create: (context) => GlobalVariable(),),
           ChangeNotifierProvider(
             create: (context) => AuthNotifier(),
           ),
