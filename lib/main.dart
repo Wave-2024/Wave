3a91af971ec23ad014dac09b5370d985eb637728
Alpha17-2 <alphaisgod1@gmail.com>
2022-02-10 19:17:44 +0530

    local changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:nexus/providers/GlobalVariable.dart';
 import 'package:nexus/providers/screenIndexProvider.dart';
 import 'package:nexus/providers/manager.dart';
 import 'package:nexus/providers/usernameProvider.dart';
