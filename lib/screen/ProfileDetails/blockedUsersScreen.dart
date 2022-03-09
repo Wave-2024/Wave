064af1d3a116764831e800c88394c29d29d3a57f
Alpha17-2 <alphaisgod1@gmail.com>
2022-03-09 20:01:45 +0530

    patch

--- lib/screen/ProfileDetails/blockedUsersScreen.dart
+++ lib/screen/ProfileDetails/blockedUsersScreen.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:nexus/models/userModel.dart';
 import 'package:nexus/providers/manager.dart';
 import 'package:nexus/utils/devicesize.dart';
 import 'package:provider/provider.dart';
@@ -9,9 +10,8 @@ class blockedUsersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    List<dynamic> blockedUsersUid = Provider.of<manager>(context, listen: false)
-        .fetchAllUsers[currentUser!.uid]!
-        .blocked;
+    var allUsers = Provider.of<manager>(context, listen: false).fetchAllUsers;
+    List<dynamic> blockedUsersUid = allUsers[currentUser!.uid]!.blocked;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -28,7 +28,134 @@ class blockedUsersScreen extends StatelessWidget {
         height: displayHeight(context),
         width: displayWidth(context),
         color: Colors.white,
+        child: ListView.builder(
+          itemCount: blockedUsersUid.length,
+          itemBuilder: (context, index) {
+            NexusUser user = allUsers[blockedUsersUid[index]]!;
+            return listTileForBlockedUser(
+              user: user,
+            );
+          },
+        ),
       ),
     );
   }
 }
+
+// TODO: Implement this class to display a list-tile
+
+class listTileForBlockedUser extends StatefulWidget {
+  final NexusUser? user;
+  listTileForBlockedUser({this.user});
+
+  @override
+  State<listTileForBlockedUser> createState() => _listTileForBlockedUserState();
+}
+
+class _listTileForBlockedUserState extends State<listTileForBlockedUser> {
+  bool isProcessing = false;
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    // Properties of list tile would be
+    // leading - Circleavatar displaying dp
+    // subtitle - title of user
+    // title - username of user
+    // trailing - unblock
+    return ListTile();
+  }
+}
+
+// Take reference from this method
+/*
+ListTile(
+                      onTap: () {
+                        if (displayList[index].uid != currentUser!.uid) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    userProfile(uid: displayList[index].uid),
+                              ));
+                        }
+                      },
+                      visualDensity:
+                          const VisualDensity(horizontal: 0, vertical: -3),
+                      leading: (displayList[index].dp != '')
+                          ? CircleAvatar(
+                              backgroundImage: CachedNetworkImageProvider(
+                                  displayList[index].dp),
+                              radius: displayWidth(context) * 0.05,
+                            )
+                          : CircleAvatar(
+                              backgroundColor: Colors.grey[200],
+                              radius: displayWidth(context) * 0.05,
+                              child: Icon(
+                                Icons.person,
+                                size: displayWidth(context) * 0.075,
+                                color: Colors.orange[400],
+                              ),
+                            ),
+                      subtitle: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Text(
+                            displayList[index].username,
+                            style: TextStyle(
+                                color: Colors.black45,
+                                fontSize: displayWidth(context) * 0.035),
+                          ),
+                          Opacity(
+                              opacity: 0.0,
+                              child: VerticalDivider(
+                                width: displayWidth(context) * 0.003,
+                              )),
+                          (displayList[index].followers.length >= 25)
+                              ? Icon(
+                                  Icons.verified,
+                                  color: Colors.orange[400],
+                                  size: displayWidth(context) * 0.048,
+                                )
+                              : const SizedBox(),
+                        ],
+                      ),
+                      title: Text(
+                        displayList[index].title,
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: displayWidth(context) * 0.038),
+                      ),
+                      trailing:  (widget.isThisMe)? InkWell(
+                          splashColor: Colors.orange,
+                          onTap: () async {
+                            if(processing){
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Please wait , processing previous request')));
+                            }
+                            else{
+                              setState(() {
+                                processing = true;
+                              });
+                              await Provider.of<manager>(context,listen: false).unFollowUser(widget.followers[index],currentUser!.uid);
+                              setState(() {
+                                primaryList.removeAt(index);
+                                processing = false;
+                              });
+
+                            }
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(5),
+                              border: Border.all(color: Colors.orange)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('Remove',style: TextStyle(fontSize: displayWidth(context)*0.03),),
+                            ),
+                          )
+                      ):const SizedBox()
+                    );
+
+*/
