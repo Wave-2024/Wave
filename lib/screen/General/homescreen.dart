cad545e742678bc72272cbd2e0237de5b9c576e5
Subhojeet Sahoo <Alphaisgod1@gmail.com>
2022-03-27 18:02:45 +0530

    patch : optimised screens

--- lib/screen/General/homescreen.dart
+++ lib/screen/General/homescreen.dart
@@ -25,143 +25,143 @@ class homescreen extends StatelessWidget {
     int screenIndex =
         Provider.of<screenIndexProvider>(context).fetchCurrentIndex;
     return Scaffold(
-      bottomNavigationBar: SizedBox(
-        height: displayHeight(context) * 0.075,
-        child: BottomNavigationBar(
-          iconSize: displayWidth(context) * 0.05,
-          onTap: (value) {
-            if (value == 2) {
-              showModalBottomSheet(
-                  context: context,
-                  builder: (ctx) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            ListTile(
-                              visualDensity: const VisualDensity(
-                                  horizontal: 0, vertical: -2),
-                              onTap: () {
-                                Navigator.pop(ctx);
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => TextPost(),
-                                    ));
-                              },
-                              title: const Text('New Text Post'),
-                            ),
-                            ListTile(
-                              visualDensity: const VisualDensity(
-                                  horizontal: 0, vertical: -2),
-                              onTap: () {
-                                Navigator.pop(ctx);
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => imagePost(),
-                                    ));
-                              },
-                              title: const Text('New Image post'),
-                            ),
-                            ListTile(
-                              visualDensity: const VisualDensity(
-                                  horizontal: 0, vertical: -2),
-                              onTap: () {
-                                Navigator.pop(ctx);
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => VideoPost(),
-                                    ));
-                              },
-                              title: const Text('New Video post'),
-                            ),
-                            ListTile(
-                              visualDensity: const VisualDensity(
-                                  horizontal: 0, vertical: -4),
-                              onTap: () {
-                                Navigator.pop(ctx);
-                              },
-                              textColor: Colors.red,
-                              title: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: const [
-                                  Icon(
-                                    Icons.close,
-                                    color: Colors.red,
-                                  ),
-                                  Opacity(
-                                    opacity: 0.0,
-                                    child: VerticalDivider(),
-                                  ),
-                                  Text(
-                                    'Cancel',
-                                    style: TextStyle(color: Colors.red),
-                                  ),
-                                ],
+        bottomNavigationBar: SizedBox(
+          height: displayHeight(context) * 0.075,
+          child: BottomNavigationBar(
+            iconSize: displayWidth(context) * 0.05,
+            onTap: (value) {
+              if (value == 2) {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (ctx) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              ListTile(
+                                visualDensity: const VisualDensity(
+                                    horizontal: 0, vertical: -2),
+                                onTap: () {
+                                  Navigator.pop(ctx);
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => TextPost(),
+                                      ));
+                                },
+                                title: const Text('New Text Post'),
                               ),
-                            ),
-                          ],
+                              ListTile(
+                                visualDensity: const VisualDensity(
+                                    horizontal: 0, vertical: -2),
+                                onTap: () {
+                                  Navigator.pop(ctx);
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => imagePost(),
+                                      ));
+                                },
+                                title: const Text('New Image post'),
+                              ),
+                              ListTile(
+                                visualDensity: const VisualDensity(
+                                    horizontal: 0, vertical: -2),
+                                onTap: () {
+                                  Navigator.pop(ctx);
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => VideoPost(),
+                                      ));
+                                },
+                                title: const Text('New Video post'),
+                              ),
+                              ListTile(
+                                visualDensity: const VisualDensity(
+                                    horizontal: 0, vertical: -4),
+                                onTap: () {
+                                  Navigator.pop(ctx);
+                                },
+                                textColor: Colors.red,
+                                title: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: const [
+                                    Icon(
+                                      Icons.close,
+                                      color: Colors.red,
+                                    ),
+                                    Opacity(
+                                      opacity: 0.0,
+                                      child: VerticalDivider(),
+                                    ),
+                                    Text(
+                                      'Cancel',
+                                      style: TextStyle(color: Colors.red),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  });
-            } else {
-              Provider.of<screenIndexProvider>(context, listen: false)
-                  .updateIndex(value);
-            }
-          },
-          elevation: 8,
-          currentIndex: screenIndex,
-          selectedItemColor: Colors.orange,
-          unselectedItemColor: Colors.black45,
-          showUnselectedLabels: true,
-          backgroundColor: Colors.white,
-          type: BottomNavigationBarType.fixed,
-          selectedLabelStyle: TextStyle(
-              fontWeight: FontWeight.w600,
-              fontSize: displayWidth(context) * 0.034),
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon((screenIndex == 0) ? Icons.home : Icons.home_outlined),
-              label: "Home",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                  (screenIndex == 1) ? Icons.search : Icons.search_outlined),
-              label: "Search",
-            ),
-            const BottomNavigationBarItem(
-              icon: Icon(Icons.add),
-              label: "New",
-            ),
-            BottomNavigationBarItem(
+                      );
+                    });
+              } else {
+                Provider.of<screenIndexProvider>(context, listen: false)
+                    .updateIndex(value);
+              }
+            },
+            elevation: 8,
+            currentIndex: screenIndex,
+            selectedItemColor: Colors.orange,
+            unselectedItemColor: Colors.black45,
+            showUnselectedLabels: true,
+            backgroundColor: Colors.white,
+            type: BottomNavigationBarType.fixed,
+            selectedLabelStyle: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: displayWidth(context) * 0.034),
+            items: [
+              BottomNavigationBarItem(
                 icon:
-                    Icon((screenIndex == 3) ? Icons.mail : Icons.mail_outline),
-                label: "Inbox",
-                backgroundColor: Colors.white),
-            BottomNavigationBarItem(
-              icon: Icon(
-                  (screenIndex == 4) ? Icons.person : Icons.person_outline),
-              label: "Profile",
-            )
+                    Icon((screenIndex == 0) ? Icons.home : Icons.home_outlined),
+                label: "Home",
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(
+                    (screenIndex == 1) ? Icons.search : Icons.search_outlined),
+                label: "Search",
+              ),
+              const BottomNavigationBarItem(
+                icon: Icon(Icons.add),
+                label: "New",
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                      (screenIndex == 3) ? Icons.mail : Icons.mail_outline),
+                  label: "Inbox",
+                  backgroundColor: Colors.white),
+              BottomNavigationBarItem(
+                icon: Icon(
+                    (screenIndex == 4) ? Icons.person : Icons.person_outline),
+                label: "Profile",
+              )
+            ],
+          ),
+        ),
+        body: IndexedStack(
+          index: screenIndex,
+          children: [
+            feedScreen(),
+            searchScreen(),
+            searchScreen(), // will never be used
+            chatScreen(),
+            profiletScreen(),
           ],
-        ),
-      ),
-      body: Container(
-        height: displayHeight(context),
-        width: displayWidth(context),
-        child: UpgradeAlert(
-          showReleaseNotes: true,
-          canDismissDialog: true,
-          minAppVersion: "1.0.0+3",
-          child: screens[screenIndex],
-        ),
-      ),
-    );
+        ));
   }
 }
