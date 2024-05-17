f6e8e8adeee7cbc26f8f7e7923e4bbb67c4212ec
Alpha17-2 <alphaisgod1@gmail.com>
2024-05-17 14:03:51 +0530

    [feat] : creates a list of circle avatar to display stories [placeholder]

--- lib/view/screens/FeedScreen/feed_screen.dart
+++ lib/view/screens/FeedScreen/feed_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:wave/utils/constants/custom_colors.dart';
 import 'package:wave/utils/constants/custom_fonts.dart';
+import 'package:wave/utils/constants/custom_icons.dart';
+import 'package:wave/utils/device_size.dart';
 
 class FeedScreen extends StatelessWidget {
   const FeedScreen({super.key});
@@ -12,15 +14,77 @@ class FeedScreen extends StatelessWidget {
       body: SafeArea(
           child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 14.0, vertical: 10),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Wave",
-              style: TextStyle(fontFamily: CustomFont.alex, fontSize: 40),
-            )
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Wave",
+                style: TextStyle(fontFamily: CustomFont.alex, fontSize: 40),
+              ),
+              Container(
+                // color: Colors.amber.shade100,
+                height: displayHeight(context) * 0.13,
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          backgroundColor: Colors.transparent,
+                          radius: 30,
+                          backgroundImage: AssetImage(CustomIcon.addStoryIcon),
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          "You",
+                          style: TextStyle(
+                              fontFamily: CustomFont.poppins, fontSize: 10),
+                        )
+                      ],
+                    ),
+                    Expanded(
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        padding: const EdgeInsets.symmetric(horizontal: 4),
+                        itemCount: 10,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 6.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.blue.shade100,
+                                ),
+                                const SizedBox(
+                                  height: 2,
+                                ),
+                                Text(
+                                  "alpha3109",
+                                  style: TextStyle(
+                                      fontFamily: CustomFont.poppins,
+                                      fontSize: 10),
+                                )
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       )),
     );
